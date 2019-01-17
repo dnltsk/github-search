d9eaee42528b8b9dff27c8ffce70cc42a2dd460d
daniel.teske <daniel.teske@mbition.io>
2019-01-17 15:27:01 +0100

    :bug: HttpControllerWebTest should use mocks

--- src/test/kotlin/org/dnltsk/githubsearch/HttpControllerWebTest.kt
+++ src/test/kotlin/org/dnltsk/githubsearch/HttpControllerWebTest.kt
@@ -1,25 +1,27 @@
 package org.dnltsk.githubsearch
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.http.HttpStatus.OK
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
 @RunWith(SpringRunner::class)
-class HttpControllerWebTest{
+@SpringBootTest
+@AutoConfigureMockMvc
+class HttpControllerWebTest {
 
     @Autowired
-    private lateinit var restTemplate: TestRestTemplate
+    private lateinit var mockMvc: MockMvc
 
     @Test
-    fun `search endpoint responds with 200OK`() {
-        val response = restTemplate.getForEntity("/search?language=kotlin", SearchResponse::class.java)
-        assertThat(response.statusCode).isEqualTo(OK)
+    fun `search endpoint works responds with 200 OK`() {
+        mockMvc.perform(get("/search?language=kotlin"))
+            .andExpect(status().isOk())
+
     }
 }
