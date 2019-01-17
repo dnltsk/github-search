9580e6d2a5b33d8331fc823f4750c704fccf2ee0
daniel.teske <daniel.teske@mbition.io>
2019-01-17 11:48:55 +0100

    :recycle: moved app configuration into Application file

--- src/main/kotlin/org/dnltsk/githubsearch/Application.kt
+++ src/main/kotlin/org/dnltsk/githubsearch/Application.kt
@@ -1,7 +1,10 @@
 package org.dnltsk.githubsearch
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class Application
@@ -10,3 +13,12 @@ fun main(args: Array<String>) {
 	SpringApplication.run(Application::class.java, *args)
 }
 
+@Configuration
+class ApplicationConfiguration{
+
+	@Bean
+	fun configureObjectMapper(): ObjectMapper {
+		return ObjectMapper().findAndRegisterModules()
+	}
+
+}
