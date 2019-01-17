d4242dfb350149138c95188431b85043cb9aa1b5
daniel.teske <daniel.teske@mbition.io>
2019-01-17 11:08:25 +0100

    :white_check_mark: search endpoint responds with 200OK

---
+++ src/test/kotlin/org/dnltsk/githubsearch/HttpControllerWebTest.kt
@@ -0,0 +1,25 @@
+package org.dnltsk.githubsearch
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus.OK
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class HttpControllerWebTest{
+
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
+
+    @Test
+    fun `search endpoint responds with 200OK`() {
+        val response = restTemplate.getForEntity("/search?query=kotlin", String::class.java)
+        assertThat(response.statusCode).isEqualTo(OK)
+    }
+}
